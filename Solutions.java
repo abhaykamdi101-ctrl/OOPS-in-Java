6d88ada1339a3abdf5ca82bf64364e99c584e136
abhaykamdi101-ctrl <abhaykamdi101@gmail.com>
2026-09-12 19:42:21 +0530

    basics in java

---
+++ Solutions.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Solutions{
+    public static void main(String args[]){
+        pen p1 = new pen();
+        p1.setColor("blue");
+        System.out.println(p1.color);
+        p1.settip(5);
+        System.out.println(p1.tip);
+        p1.setStyle("aqua blue");     
+    }
+}
+class pen {
+    String color;
+    int tip;
+    String style;
+    void setColor(String newcolor){
+        color = newcolor;        
+    } 
+    void settip(int newtip){
+        tip = newtip;
+    }
+    void setStyle(String newStyle){
+        style = newStyle;
+        System.out.println(style);
+    }
+}
+class BankAccount{
+    String name;
+    String lname;
+    public void setInfo(String na , String lna){
+       System.out.print(" hello");
+       name = na;
+       lname = lna;
+        System.out.print(name);
+         System.out.print(lname);
+    }
+}
